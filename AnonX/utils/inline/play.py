6f0291eadbfd8e3b6c060d59661872c790b9df94
MR PERFECT OP <147732275+dattuxd@users.noreply.github.com>
2023-12-05 20:20:03 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -63,7 +63,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="◉ ᴏᴡɴᴇʀ ", url=f"https://t.me/MrPerfectXd"
+                text="◉ ᴏᴡɴᴇʀ ", url=f"https://t.me/MeAwara"
             )
         ],
         [
